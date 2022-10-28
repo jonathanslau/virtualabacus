440fd3ca118eb0cf9af029c0b5744208b1dba9d9
Jonathan Au <58671823+jonathanslau@users.noreply.github.com>
2022-10-28 16:36:17 -0400

    Update app.js
    corrected app.use command to point to the correct build directory

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
