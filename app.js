b24720292663758b0fd737acddbab49cb17bb50f
Jonathan Au <58671823+jonathanslau@users.noreply.github.com>
2022-10-28 16:17:05 -0400

    test

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(process.env.PORT || 4000, function () {
-    console.log('Node app is working!');
+    // console.log('Node app is working!');
 });
